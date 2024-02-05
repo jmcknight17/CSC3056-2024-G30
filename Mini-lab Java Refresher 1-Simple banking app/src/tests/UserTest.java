cfa62ba2e7522ddc13bbccf143b07088b2c20932
JackLad14 <jack.kilburn123@gmail.om>
2024-02-05 18:19:41 +0000

    TestUtils implementation

---
+++ Mini-lab Java Refresher 1-Simple banking app/src/tests/UserTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import Model.User;
+import Utils.TestUtils;
+
+public class UserTest {
+
+	public static void testUserConstructor() {
+		String test_username = "mike";
+		String test_password = "my_password";
+		String test_first_name = "Mike";
+		String test_last_name = "Smith";
+		String test_mobile_number = "07771234567";
+		
+		User testUser = new User(test_username, test_password, test_first_name, test_last_name, test_mobile_number);
+		
+		System.out.println("Starting the assertions of the test method: testUserConstructor");
+		
+		if (testUser.getUsername() == test_username)
+			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC1 passed" + TestUtils.TEXT_COLOR_RESET);
+		else
+			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC1 failed: username did not match" + TestUtils.TEXT_COLOR_RESET);
+
+		if (testUser.getPassword() == test_password)
+			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC2 passed" + TestUtils.TEXT_COLOR_RESET);
+		else
+			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC2 failed: password did not match" + TestUtils.TEXT_COLOR_RESET);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
