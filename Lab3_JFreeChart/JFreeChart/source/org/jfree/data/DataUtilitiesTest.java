b8ea37584cd017a632b9eaaa59c9c868904d04ec
jmcknight17 <jmcknight17@qub.ac.uk>
2024-03-12 16:55:37 +0000

    Adding_Previous_Test_Classes

---
+++ Lab3_JFreeChart/JFreeChart/source/org/jfree/data/DataUtilitiesTest.java
@@ -0,0 +1,403 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+
+import java.security.InvalidParameterException;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.Values2D;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataUtilitiesTest extends DataUtilities {
+
+	private Values2D values2D;
+	
+	@Before
+	public void setUp() throws Exception {
+
+	}
+	
+	//Start of CalculateColumnTotal Test Cases
+	@Test public void testCalculateColumnTotalNegativeIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(1, 2.7, 3);
+		data.addValue(4, 5, -6);
+		data.addValue(7, 8.4, 9);
+		
+		assertEquals("0 should be returned", 0, DataUtilities.calculateColumnTotal(data, -1), 0.00000001d);
+	}
+	
+	@Test public void testCalculateColumnTotalInvalidHigherIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(1, 2, 3);
+		data.addValue(4, 5, 6);
+		data.addValue(7, 8, 9);
+
+		assertEquals("0 should be returned", 0, DataUtilities.calculateColumnTotal(data, 4), 0.00000001d);
+	}
+	
+	@Test public void testCalculateColumnTotalValidLowerIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(0, 2.1, 3);
+		data.addValue(4.2, 5, 6);
+		data.addValue(7, 8, 9);
+		
+
+		assertEquals("11.2 should be returned", 11.2, DataUtilities.calculateColumnTotal(data, 0), 0.00000001d);
+	}
+	
+	@Test public void testCalculateColumnTotalValidUpperIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(1, 2, 3);
+		data.addValue(4, 5, 6.1);
+		data.addValue(7, 8, 0);
+		
+
+		assertEquals("9.1 should be returned", 9.1, DataUtilities.calculateColumnTotal(data, 2), 0.00000001d);
+
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCalculateColumnTotalNullWithNegativeIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(null, 2, 5);
+		data.addValue(1, null, 2);
+		data.addValue(7, 3, 5);
+		
+		DataUtilities.calculateColumnTotal(data, -1);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCalculateColumnTotalNullWithValidLowerIndex() {
+		DataUtilities.calculateColumnTotal(null, 0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCalculateColumnTotalNullWithValidUpperIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(null, 2, 5);
+		data.addValue(1, null, 2);
+		data.addValue(7, 3, 5);
+		
+		DataUtilities.calculateColumnTotal(data, 2);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCalculateColumnTotalNullWithInvalidHigherIndex() {
+		DataUtilities.calculateColumnTotal(null, 3);
+	}
+	//End of CalculateColumnTotal Test Cases
+	
+	
+	
+	
+	
+	//Start of CalculateRowTotal Test Cases
+	@Test public void testCalculateRowTotalNegativeIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(1, 2.7, 3);
+		data.addValue(4, 5, -6);
+		data.addValue(7, 8.4, 9);
+		
+		assertEquals("0 should be returned", 0, DataUtilities.calculateRowTotal(data, -1), 0.00000001d);
+	}
+	
+	@Test public void testCalculateRowTotalInvalidHigherIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(1, 2, 3);
+		data.addValue(4, 5, 6);
+		data.addValue(7, 8, 9);
+		
+		assertEquals("0 should be returned", 0, DataUtilities.calculateRowTotal(data, 4), 0.00000001d);
+	}
+	
+	@Test public void testCalculateRowTotalValidLowerIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(0, 2.1, 3);
+		data.addValue(4.2, 5, 6);
+		data.addValue(7, 8, 9);
+		
+
+		assertEquals("5.1 should be returned", 5.1, DataUtilities.calculateRowTotal(data, 0), 0.00000001d);
+	}
+	
+	@Test public void testCalculateRowTotalValidUpperIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(1, 2, 3);
+		data.addValue(4, 5, 6.1);
+		data.addValue(7.7, 8, 0);
+		
+
+		assertEquals("15.7 should be returned", 15.7, DataUtilities.calculateRowTotal(data, 2), 0.00000001d);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCalculateRowTotalNullWithNegativeIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(null, 2, 5);
+		data.addValue(1, null, 2);
+		data.addValue(7, 3, 5);
+		
+		DataUtilities.calculateRowTotal(data, -1);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCalculateRowTotalNullWithValidLowerIndex() {
+		DataUtilities.calculateRowTotal(null, 0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCalculateRowTotalNullWithValidUpperIndex() {
+		DefaultKeyedValues2D data = new DefaultKeyedValues2D();
+		data.addValue(null, 2, 5);
+		data.addValue(1, null, 2);
+		data.addValue(7, 3, 5);
+		
+		DataUtilities.calculateRowTotal(data, 2);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCalculateRowTotalNullWithInvalidHigherIndex() {
+		DataUtilities.calculateRowTotal(null, 3);
+	}
+	//End of CalculateRowTotal Test Cases
+	
+	
+	
+	
+	
+	//Start of CreateNumberArray Test Cases
+	@Test public void testCreateNumberArrayValidData() {
+		double[] data = {1.0, -2.0, 0};
+		Number[] expected = {1.0, -2.0, 0};
+		
+		assertArrayEquals("Number Array of {1.0, -2.0, 0} should be returned", expected, DataUtilities.createNumberArray(data));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateNumberArrayNullData() {
+		double[] data = null;
+		
+		DataUtilities.createNumberArray(data);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCreateNumberArrayNullEntryData() {
+		double[] data = {(Double)null, 0.0};
+		
+		DataUtilities.createNumberArray(data);
+	}
+	
+	@Test public void testCreateNumberArrayEmptyData() {
+		double[] data = {};
+		Number[] expected = {};
+		
+		assertArrayEquals("Number Array of {} should be returned", expected, DataUtilities.createNumberArray(data));
+	}
+	
+	@Test public void testCreateNumberArraySingleEntryData() {
+		double[] data = {1.6};
+		Number[] expected = {1.6};
+		
+		assertArrayEquals("Number Array of {1.6} should be returned", expected, DataUtilities.createNumberArray(data));
+	}
+	
+	@Test public void testCreateNumberArraySpecialDoubleData() {
+        double[] data = {Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NaN};
+        Number[] expected = {Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NaN};
+        
+		assertArrayEquals("Number Array of {POSITIVE_INFINITY, NEGATIVE_INFINITY, NaN} should be returned", 
+				expected, DataUtilities.createNumberArray(data));
+	}
+	//End of CreateNumberArray Test Cases
+	
+	
+	
+	
+	
+	//Start of CreateNumberArray2D Test Cases
+	@Test public void testCreateNumberArray2dValidData() {
+		double[][] data = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
+		Number[][] expected = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
+		
+		assertArrayEquals("2D Number Array of {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}} should be returned", 
+				expected, DataUtilities.createNumberArray2D(data));
+	}
+	
+	@Test public void testCreateNumberArray2dSpecialDoubleData() {
+        double[][] data = {{Double.NaN, Double.POSITIVE_INFINITY}, {9.9, 10}};
+        Number[][] expected = {{Double.NaN, Double.POSITIVE_INFINITY}, {9.9, 10}};
+        
+		assertArrayEquals("2D Number Array of {{Double.NaN, Double.POSITIVE_INFINITY}, {9.9, 10}} should be returned", expected, DataUtilities.createNumberArray2D(data));
+	}
+	
+	@Test public void testCreateNumberArray2dDifferingRowData() {
+        double[][] data = {{-1.3, 6.5}, {1.0, 6.4, 66.6}};
+        Number[][] expected = {{-1.3, 6.5}, {1.0, 6.4, 66.6}};
+        
+		assertArrayEquals("2D Number Array of {{-1.3, 6.5}, {1.0, 6.4, 66.6}} should be returned", 
+				expected, DataUtilities.createNumberArray2D(data));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateNumberArray2dNullData() {
+        double[][] input = null;
+        DataUtilities.createNumberArray2D(input);
+	}
+	
+	@Test(expected = NullPointerException .class)
+	public void testCreateNumberArray2dNullEntryData() {
+        double[][] input = {{5.5, (Double) null}, {1.1, 2.2}};
+        DataUtilities.createNumberArray2D(input);
+	}
+	
+	@Test public void testCreateNumberArray2dEmptyData() {
+        double[][] data = {{}, {}};
+        Number[][] expected = {{}, {}};
+
+		assertArrayEquals("2D Number Array of {{}, {}} should be returned", expected, DataUtilities.createNumberArray2D(data));
+	}
+	
+	@Test public void testCreateNumberArray2dSingleArrayData() {
+        double[][] data = {{1.1, 2.2, 3.3, 4.4}};
+        Number[][] expected = {{1.1, 2.2, 3.3, 4.4}};
+        
+		assertArrayEquals("2D Number Array of {{1.1, 2.2, 3.3, 4.4}} should be returned", 
+				expected, DataUtilities.createNumberArray2D(data));
+	}
+	
+	@Test public void testCreateNumberArray2dSingleEntryData() {
+        double[][] data = {{1.1}, {1.1, 2.2, 3.3, 4.4}};
+        Number[][] expected = {{1.1}, {1.1, 2.2, 3.3, 4.4}};
+        
+		assertArrayEquals("2D Number Array of {{1.1}, {1.1, 2.2, 3.3, 4.4}} should be returned", 
+				expected, DataUtilities.createNumberArray2D(data));
+	}
+	
+	@Test public void testCreateNumberArray2dMaxDoubleData() {
+        double[][] data = {{Double.MAX_VALUE, -Double.MAX_VALUE}, {Double.MIN_VALUE, -Double.MIN_VALUE}};
+        Number[][] expected = {{Double.MAX_VALUE, -Double.MAX_VALUE}, {Double.MIN_VALUE, -Double.MIN_VALUE}};
+        
+		assertArrayEquals("2D Number Array of {{MAX_VALUE, -MAX_VALUE}, {MIN_VALUE, MIN_VALUE}} should be returned", expected, DataUtilities.createNumberArray2D(data));
+	}
+	//End of CreateNumberArray2D Test Cases
+	
+	
+	
+	
+	
+	//Start of GetCummulativePercentages Test Cases
+	@Test public void testGetCumulativePercentagesEmptyData() {
+		DefaultKeyedValues data = new DefaultKeyedValues();
+		DefaultKeyedValues expected = new DefaultKeyedValues();
+        
+	    assertEquals("KeyedValues instance of {} should be returned", expected, DataUtilities.getCumulativePercentages(data));
+	}
+	
+	@Test public void testGetCumulativePercentagesSinglePositiveData() {
+        DefaultKeyedValues data = new DefaultKeyedValues();
+        data.addValue("1", 100);
+        
+        DefaultKeyedValues expected = new DefaultKeyedValues();
+        data.addValue("1", 1.0);
+
+	    assertEquals("KeyedValues instance of {1, 1.0} should be returned", 
+	    		expected, DataUtilities.getCumulativePercentages(data));
+	}
+	
+	@Test public void testGetCumulativePercentagesValidData() {
+		DefaultKeyedValues data = new DefaultKeyedValues();
+        data.addValue("1", 20);
+        data.addValue("2", 20);
+        
+        DefaultKeyedValues expected = new DefaultKeyedValues();
+        expected.addValue("1", 0.5);
+        expected.addValue("2", 1.0);
+        
+	    assertEquals("KeyedValues instance of {{1, 0.5}, {2, 1.0}} should be returned", 
+	    		expected, DataUtilities.getCumulativePercentages(data));
+	}
+	
+	@Test public void testGetCumulativePercentagesContainsZeroData() {
+		DefaultKeyedValues data = new DefaultKeyedValues();
+        data.addValue("1", 30);
+        data.addValue("2", 30);
+        data.addValue("3", 0);
+        
+        DefaultKeyedValues expected = new DefaultKeyedValues();
+        expected.addValue("1", 0.5);
+        expected.addValue("2", 1.0);
+        expected.addValue("3", 1.0);
+        
+	    assertEquals("KeyedValues instance of {{1, 0.5}, {2, 1.0}, {3, 1.0}} should be returned", 
+	    		expected, DataUtilities.getCumulativePercentages(data));
+	}
+	
+	@Test public void testGetCumulativePercentagesContainsNegativeData() {
+		DefaultKeyedValues data = new DefaultKeyedValues();
+        data.addValue("1", 20);
+        data.addValue("2", -10);
+        data.addValue("3", 90);
+        
+        DefaultKeyedValues expected = new DefaultKeyedValues();
+        expected.addValue("1", 0.2);
+        expected.addValue("2", 0.1);
+        expected.addValue("3", 1.0);
+        
+	    assertEquals("KeyedValues instance of {{1, 0.2}, {2, 0.1}, {3, 1.0}} should be returned", 
+	    		expected, DataUtilities.getCumulativePercentages(data));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetCumulativePercentagesNullData() {
+		DataUtilities.getCumulativePercentages(null);
+	}
+	//End of GetCummulativePercentages Test Cases
+	
+	
+	
+	@After
+	public void tearDown() throws Exception {
+		values2D = null;
+	}
+	
+	
+	
+	/*
+	private Values2D values2D;
+	@Before
+	public void setUp() throws Exception {
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(1, 0, 0);
+		testValues.addValue(4 , 1, 0);
+	}
+
+	@Test
+	public void testValidDataAndColumnColumnTotal() {
+		assertEquals("Wrong sum returned. It should be 5.0", 
+				5.0, DataUtilities.calculateColumnTotal(values2D, 0), 0.00000001d);
+	}
+	
+	@Test 
+	public void testNullDataColumnTotal() {
+		try {
+			DataUtilities.calculateColumnTotal(null, 0);
+			fail("No exception thrown. Expected to throw an exception of type IllegalArgumentException");
+		}
+		catch(Exception e) {
+			assertTrue("Incorrect exception type thrown", 
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		values2D = null;
+	}
+	*/
+}
---
+++ Lab3_JFreeChart/JFreeChart/source/org/jfree/data/RangeTest.java
@@ -0,0 +1,345 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+import java.security.InvalidParameterException;
+import junit.framework.TestCase; 
+import org.jfree.data.Range;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RangeTest {
+	
+	private Range rangeObjectUnderTest;
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	//Start of Intersects tests 
+	@Test public void testIntersectsBaseRangeWithIntersectingRange() {
+        Range baseRange = new Range(1, 10);
+        assertTrue("The two ranges should intersect", baseRange.intersects(1,3));
+    }
+
+    @Test public void testIntersectsBaseRangeWithNonIntersectingRangeContainingOneOnLeftValue() {
+        Range baseRange = new Range(1, 10);
+        assertFalse("The two ranges should not intersect", baseRange.intersects(0,1));
+    }
+    
+    @Test public void testIntersectsBaseRangeWithIntersectingRangeContainingOneHighestBoundaryValue() {
+        Range baseRange = new Range(1, 10);
+        assertTrue("The two ranges should intersect", baseRange.intersects(9,10));
+    }
+
+    @Test public void testIntersectsBaseRangeWithNonIntersectingRangeContainingOneOnRightValue() {
+        Range baseRange = new Range(1, 10);
+        assertFalse("The two ranges should not intersect", baseRange.intersects(10,11));
+    }
+
+    @Test public void testIntersectsBaseRangeWithIntersectingRangeContainingSameValues() {
+        Range baseRange = new Range(1, 10);
+        assertTrue("The two ranges should intersect", baseRange.intersects(5,5));
+    }
+    
+    @Test public void testIntersectsBaseRangeWithNonIntersectingRangeContainingTwoOnLeftValues() {
+        Range baseRange = new Range(1, 10);
+        assertFalse("The two ranges should not intersect", baseRange.intersects(0,0));
+    }
+    
+    @Test public void testIntersectsBaseRangeWithNonIntersectingRangeContainingTwoOnRightValues() {
+        Range baseRange = new Range(1, 10);
+        assertFalse("The two ranges should not intersect", baseRange.intersects(11,11));
+    }
+    
+    @Test public void testIntersectsBaseRangeWithIntersectingRangeContainingTwoLowestBoundaryValues() {
+        Range baseRange = new Range(1, 10);
+        assertTrue("The two ranges should intersect", baseRange.intersects(1,1));
+    }
+    
+    @Test public void testIntersectsBaseRangeWithIntersectingRangeContainingTwoHighestBoundaryValues() {
+        Range baseRange = new Range(1, 10);
+        assertTrue("The two ranges should intersect", baseRange.intersects(10,10));
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testIntersectsBaseRangeWithIllegalIntersectingRange() {
+        Range baseRange = new Range(1, 10);
+        baseRange.intersects(5,1);
+    }
+
+    @Test public void testIntersectsBaseRangeWithMatchingIntersectingRange() {
+        Range baseRange = new Range(1, 10);
+        assertTrue("The range should intersect with itself", baseRange.intersects(1,10));
+    }
+  //End of Intersects tests 
+
+  //Start of Shift tests 
+    
+    @Test public void testShiftBaseRangeWithZeroDelta() {
+    	Range baseRange = new Range(1.0,3.0);
+    	Range result = Range.shift(baseRange, 0);
+    	Range expectedRange = new Range(1.0,3.0);
+    	
+    	assertEquals("Shift: Did not return the expected output of (1.0, 3.0)", expectedRange, result);
+    }
+    
+    @Test public void testShiftBaseRangeContainingNegativeWithPositiveDeltaCausingZeroCrossing() {
+    	Range baseRange = new Range(-2.0, 4.0);
+    	Range result = Range.shift(baseRange, 3.0);
+    	Range expectedRange = new Range(0.0,7.0);
+    	
+    	assertEquals("Shift: Did not return the expected output of (0.0, 7.0)", expectedRange, result);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testShiftNullBaseRangeWithValidDelta() {
+        Range.shift(null, 1.0);
+    }
+    
+    @Test public void testShiftBaseRangeContainingTwoNegativeValuesWithNegativeDelta() {
+    	Range baseRange = new Range(-7.0, -3.0);
+    	Range result = Range.shift(baseRange, -5.0);
+    	Range expectedRange = new Range(-12.0,-8.0);
+    	
+    	assertEquals("Shift: Did not return the expected output of (-12.0,-8.0)", expectedRange, result);
+    }
+    
+    @Test public void testShiftBaseRangeWithNegativeDeltaCausingZeroCrossing() {
+    	Range baseRange = new Range(5.0, 12.0);
+    	Range result = Range.shift(baseRange, -7.0);
+    	Range expectedRange = new Range(0.0,5.0);
+    	
+    	assertEquals("Shift: Did not return the expected output of (0.0, 5.0)", expectedRange, result);
+    }
+    
+    @Test public void testShiftBaseRangeContainingMaxValueWithPositiveDelta() {
+    	Range baseRange = new Range(Double.MAX_VALUE, Double.MAX_VALUE);
+    	Range result = Range.shift(baseRange, 100);
+    	Range expectedRange = new Range (Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
+    	assertEquals("Shift: Did not return the expected output of (Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY)"
+    			, expectedRange, result);
+    }
+    
+    @Test public void testShiftBaseRangeContainingMinValueWithNegativeDelta() {
+    	Range baseRange = new Range(Double.MIN_VALUE, Double.MIN_VALUE);
+    	Range result = Range.shift(baseRange, 100);
+    	Range expectedRange = new Range (Double.NEGATIVE_INFINITY,Double.NEGATIVE_INFINITY);
+    	
+    	assertEquals("Shift: Did not return the expected output of (Double.NEGATIVE_INFINITY,Double.NEGATIVE_INFINITY)"
+    			, expectedRange, result);
+    }
+    
+  //End of shift tests
+    
+  //Start of combine tests 
+    
+    @Test public void testCombineBothRangesOverlapping() {
+    	Range range1 = new Range(3.0, 5.0);
+    	Range range2 = new Range(3.0, 8.0);
+    	Range result = Range.combine(range1, range2);
+    	Range expectedRange = new Range(3.0, 8.0);
+    	
+    	assertEquals("Combine: Did not return the expected range of (3, 8)", expectedRange, result);
+    }
+    
+    @Test public void testCombineOneEmptyRangeWithNonOverlapping() {
+    	Range range1 = new Range(9.0, 9.0);
+    	Range range2 = new Range(3.0, 8.0);
+    	Range result = Range.combine(range1, range2);
+    	Range expectedRange = new Range(3.0, 8.0);
+    	
+    	assertEquals("Combine: Did not return the expected range of (3, 8)", expectedRange, result);
+    }
+    
+    @Test public void testCombineNullWithRange() {
+    	Range range2 = new Range(4.0, 9.0);
+    	Range result = Range.combine(null, range2);
+    	Range expectedRange = new Range(4.0, 9.0);
+    	
+    	assertEquals("Combine: Did not return the expected range of (4, 9)", expectedRange, result);
+    }
+    
+    @Test public void testCombineNonOverlappingWithOneEmptyRange() {
+    	Range range1 = new Range(5.0, 10.0);
+    	Range range2 = new Range(2.0, 2.0);
+    	Range result = Range.combine(range1, range2);
+    	Range expectedRange = new Range(5.0, 10.0);
+    	
+    	assertEquals("Combine: Did not return the expected range of (5, 10)", expectedRange, result);
+    }
+    
+    @Test public void testCombineRangeWithNull() {
+    	Range range1 = new Range(1.0, 10.0);
+    	Range result = Range.combine(range1, null);
+    	Range expectedRange = new Range(1.0, 10.0);
+    	
+    	assertEquals("Combine: Did not return the expected range of (1, 10)", expectedRange, result);
+    }
+  
+    @Test public void testCombineContainingMaxValueWithOverlappingRange() {
+    	Range range1 = new Range(7, Double.MAX_VALUE);
+    	Range range2 = new Range(1.0, 8.0);
+    	Range result = Range.combine(range1, range2);
+    	Range expectedRange = new Range(1,Double.MAX_VALUE);
+    	
+    	assertEquals("Combine: Did not return the expected range of (1, Double.MAX_VALUE)", expectedRange, result);
+    }
+    
+    @Test public void testCombineOverlappingRangeWithRangeContainingMin() {
+    	Range range1 = new Range(1, 12);
+    	Range range2 = new Range(Double.MIN_VALUE, 10);
+    	Range result = Range.combine(range1, range2);
+    	Range expectedRange = new Range(Double.MIN_VALUE,12);
+    	
+    	assertEquals("Combine: Did not return the expected range of (Double.MIN_VALUE, 12)", expectedRange, result);
+    }
+    
+    @Test public void testCombineOverlappingRangeWithRangeContainingMax() {
+    	Range range1 = new Range(1, 4);
+    	Range range2 = new Range(0, Double.MAX_VALUE);
+    	Range result = Range.combine(range1, range2);
+    	Range expectedRange = new Range(0, Double.MAX_VALUE);
+    	
+    	assertEquals("Combine: Did not return the expected range of (0, Double.MAX_VALUE)", expectedRange, result);
+    }
+    
+    @Test public void testCombineTwoNullRanges() {
+    	Range result = Range.combine(null, null);
+    	Range expectedRange = null;
+    	assertEquals("Combine: Did not return the expected range of (0, Double.MAX_VALUE)", expectedRange, result);
+    }
+  //End of combine tests   
+    
+  //Start of expand tests 
+    
+    @Test public void testExpandRangeWithPositiveMargins() {
+    	Range baseRange = new Range(2,6);
+    	Range result = Range.expand(baseRange, 0.75, 0.5);
+    	Range expectedRange = new Range (-1,8);
+    	
+    	assertEquals("Expand: Did not return the expected range of (-1, 8)", expectedRange, result);
+    }
+    
+    @Test public void testExpandRangeWithDoubleNegativeMarginsCausingEmptyRange() {
+    	try {
+    		Range baseRange = new Range(1,11);
+        	Range result = Range.expand(baseRange, -0.5, -0.5);
+        	Range expectedRange = new Range (6,6);
+        	
+        	assertEquals("Expand: Did not return the expected range of (6, 6)", expectedRange, result);
+    	}
+    	catch(Exception e) {
+    		fail("Expand: Did not return the expected range of (6, 6) Error: " + e.getMessage() );
+    	}
+    	
+    }
+    
+    @Test public void testExpandEmptyRangeWithPositiveAndZeroMargins() {
+    	Range baseRange = new Range(2,2);
+    	Range result = Range.expand(baseRange, 1, 0);
+    	Range expectedRange = new Range (2,2);
+    	
+    	assertEquals("Expand: Did not return the expected range of (2, 2)", expectedRange, result);
+    }
+    
+    @Test public void testExpandRangeWithZeroAndPositiveMargins() {
+    	Range baseRange = new Range(1,5);
+    	Range result = Range.expand(baseRange, 0, 0.75);
+    	Range expectedRange = new Range (1,8);
+    	
+    	assertEquals("Expand: Did not return the expected range of (1, 8)", expectedRange, result);
+    }
+    
+    @Test public void testExpandRangeWithNegativeAndPositiveMargins() {
+    	Range baseRange = new Range(6,10);
+    	Range result = Range.expand(baseRange, -0.25, 1);
+    	Range expectedRange = new Range (7,14);
+    	
+    	assertEquals("Expand: Did not return the expected range of (7, 14)", expectedRange, result);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testExpandRangeWithNegativeLowerMarginCausingRangeToFlip() {
+    	Range baseRange = new Range(6,10);
+    	Range.expand(baseRange, -1.25, 0);
+    }
+    
+    @Test public void testExpandRangeWithPositiveAndZeroMargins() {
+    	Range baseRange = new Range(10, 20);
+    	Range result = Range.expand(baseRange, 0.5, 0);
+    	Range expectedRange = new Range (5,20);
+    	
+    	assertEquals("Expand: Did not return the expected range of (5, 20)", expectedRange, result);
+    }
+    
+    @Test public void testExpandRangeWithPositiveAndNegativeMargins() {
+    	Range baseRange = new Range(10,20);
+    	Range result = Range.expand(baseRange, 0.8, -0.2);
+    	Range expectedRange = new Range (2,18);
+    	
+    	assertEquals("Expand: Did not return the expected range of (2, 18)", expectedRange, result);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testExpandRangeWithNegativeUpperMarginCausingRangeToFlip() {
+    	Range baseRange = new Range(10,20);
+    	Range.expand(baseRange, 0, -1.2);
+    }
+  //End of expand tests
+    
+  //Start of expandToInclude tests 
+	@Test public void testExpandToIncludeRangeWithValueWithinRange() {
+		Range baseRange = new Range(3, 6);
+		Range result = Range.expandToInclude(baseRange, 5);
+		Range expectedRange = new Range(3, 6);
+		
+		assertEquals("ExpandToInclude: Did not return the expected range of (3, 6)", expectedRange, result);
+	}
+	
+	@Test public void testExpandToIncludeRangeWithValueLeftOfLowerBound() {
+		Range baseRange = new Range(6, 9);
+		Range result = Range.expandToInclude(baseRange, 4);
+		Range expectedRange = new Range(4, 9);
+		
+		assertEquals("ExpandToInclude: Did not return the expected range of (4, 9)", expectedRange, result);
+	}
+	
+	@Test public void testExpandToIncludeRangeWithValueRightOfUpperBound() {
+		Range baseRange = new Range(1, 3);
+		Range result = Range.expandToInclude(baseRange, 5);
+		Range expectedRange = new Range(1, 5);
+		
+		assertEquals("ExpandToInclude: Did not return the expected range of (1, 5)", expectedRange, result);
+	}
+	
+	@Test public void testExpandToIncludeNullRangeWithPositiveValue() {
+		Range result = Range.expandToInclude(null, 8);
+		Range expectedRange = new Range(8, 8);
+		
+		assertEquals("ExpandToInclude: Did not return the expected range of (8, 8)", expectedRange, result);
+	}
+	
+	@Test public void testExpandToIncludeRangeWithValueEqualToLowerBound() {
+		Range baseRange = new Range(5, 10);
+		Range result = Range.expandToInclude(baseRange, 5);
+		Range expectedRange = new Range(5, 10);
+		
+		assertEquals("ExpandToInclude: Did not return the expected range of (5, 10)", expectedRange, result);
+	}
+	
+	@Test public void testExpandToIncludeRangeWithValueEqualToUpperBound() {
+		Range baseRange = new Range(5, 10);
+		Range result = Range.expandToInclude(baseRange, 10);
+		Range expectedRange = new Range(5, 10);
+		
+		assertEquals("ExpandToInclude: Did not return the expected range of (5, 10)", expectedRange, result);
+	}
+	//End of Expand tests
+	
+	
+	public void tearDown() throws Exception{	
+	
+	}
+
+}
